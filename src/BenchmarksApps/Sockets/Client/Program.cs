768c819bd5e7ce60e972ed12d323775ea37991a6
Natalia Kondratyeva <knatalia@microsoft.com>
2021-12-02 23:24:19 +0100

    Sockets http benchmark (regression repro)

---
+++ src/BenchmarksApps/Sockets/Client/Program.cs
@@ -0,0 +1,102 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using Microsoft.Crank.EventSources;
+using System;
+using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+using System.Linq;
+using System.Collections.Generic;
+
+Console.WriteLine(string.Join(" ", args));
+var ip = IPAddress.Parse(args[0]);
+var port = int.Parse(args[1]);
+var threads = int.Parse(args[2]);
+Console.WriteLine($"ip={ip}; port={port}; threads={threads}");
+
+PrintActiveTcpConnections(port, "start");
+
+string query = $"GET / HTTP/1.1\r\nHost:{ip}:{port}\r\n\r\n";
+byte[] request = Encoding.ASCII.GetBytes(query);
+
+int W = 4 * 3_200 / threads;
+int K = 4 * 64_000 / threads;
+
+var tasks = new List<Task<double>>(threads);
+for (int i = 0; i < threads; ++i)
+{
+    tasks.Add(MeasureRPS());
+}
+
+var threadRpsValues = await Task.WhenAll(tasks);
+
+PrintActiveTcpConnections(port, "end");
+
+double rps = 0;
+foreach (var threadRpsValue in threadRpsValues)
+{
+    rps += threadRpsValue;
+}
+
+RegisterAndLog("http/rps/mean", "Mean RPS", rps);
+
+// ---
+
+async Task<double> MeasureRPS()
+{
+    byte[] receiveBuffer = new byte[64 * 1024];
+
+    for (int i = 0; i < W; ++i)
+    {
+        await Request(receiveBuffer);
+    }
+
+    var stopwatch = Stopwatch.StartNew();
+    for (int i = 0; i < K; ++i)
+    {
+        await Request(receiveBuffer);
+    }
+    var elapsedTicks = stopwatch.ElapsedTicks;
+
+    var elapsed = elapsedTicks * 1.0 / Stopwatch.Frequency;
+    return K * 1.0 / elapsed;
+}
+
+async Task Request(byte[] buf)
+{
+    Memory<byte> memory = buf;
+
+    using Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+    await client.ConnectAsync(new IPEndPoint(ip, port));
+    await client.SendAsync(request, SocketFlags.None);
+    int total = 0;
+    while (true)
+    {
+        int num = await client.ReceiveAsync(memory[total..], SocketFlags.None);
+        if (num == 0)
+        {
+            break;
+        }
+        total += num;
+    }
+    client.Shutdown(SocketShutdown.Both);
+    client.Dispose();
+}
+
+
+void PrintActiveTcpConnections(int port, string prefix)
+{
+    IPGlobalProperties properties = IPGlobalProperties.GetIPGlobalProperties();
+    TcpConnectionInformation[] connections = properties.GetActiveTcpConnections();
+    RegisterAndLog($"env/systemconnections/all/{prefix}", $"[{prefix}] Active TCP connections", connections.Length);
+    RegisterAndLog($"env/systemconnections/port/{prefix}", $"[{prefix}] Active TCP connections to {port}", connections.Count(x => x.RemoteEndPoint.Port == port));
+}
+
+void RegisterAndLog(string name, string description, double value)
+{
+    BenchmarksEventSource.Register(name, Operations.Max, Operations.Max, description, description, "n0");
+    BenchmarksEventSource.Measure(name, value);
+    Console.WriteLine($"{description}: {value}");
+}
---
+++ src/BenchmarksApps/Sockets/Server/Program.cs
@@ -0,0 +1,102 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System;
+using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+using System.Linq;
+using Microsoft.Crank.EventSources;
+using System.Buffers;
+using System.Collections.Generic;
+
+Console.WriteLine(string.Join(" ", args));
+var ip = IPAddress.Parse(args[0]);
+var port = int.Parse(args[1]);
+Console.WriteLine($"ip={ip}; port={port}");
+
+PrintActiveTcpConnections(port, "start");
+
+string responseText = $"HTTP/1.1 200 OK\r\nContent-Length: 12\r\n\r\nHello World!";
+byte[] response = Encoding.ASCII.GetBytes(responseText);
+
+using Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+server.Bind(new IPEndPoint(ip, port));
+
+server.Listen();
+
+Console.WriteLine("Application started");
+
+var tasksRoot = new List<Task>(1_000_000); // don't bother awaiting, but need to prevent GC
+
+while (true)
+{
+    Socket accept = await server.AcceptAsync();
+    accept.NoDelay = true;
+    tasksRoot.Add(HandleRequest(accept));
+}
+
+// ---
+
+async Task HandleRequest(Socket socket)
+{
+    byte[]? receiveBuffer = null;
+    try
+    {
+        receiveBuffer = ArrayPool<byte>.Shared.Rent(64*1024);
+        Memory<byte> memory = receiveBuffer;
+        int total = 0;
+        while (true)
+        {
+            int num = await socket.ReceiveAsync(memory[total..], SocketFlags.None);
+            total += num;
+            if (num == 0)
+            {
+                Console.WriteLine("Unexpected EOS");
+                throw new Exception("Unexpected EOS");
+            }
+            if (total >= 4 &&
+                receiveBuffer[total - 4] == (byte)'\r' &&
+                receiveBuffer[total - 3] == (byte)'\n' &&
+                receiveBuffer[total - 2] == (byte)'\r' &&
+                receiveBuffer[total - 1] == (byte)'\n')
+            {
+                break;
+            }
+        }
+
+        await socket.SendAsync(response, SocketFlags.None);
+    }
+    finally
+    {
+        if (receiveBuffer != null)
+        {
+            ArrayPool<byte>.Shared.Return(receiveBuffer);
+        }
+        // the way Kestrel does it
+        try
+        {
+            socket.Shutdown(SocketShutdown.Both);
+        }
+        catch
+        { 
+            // ignore 
+        }
+        socket.Dispose();
+    }
+}
+
+
+void PrintActiveTcpConnections(int port, string prefix)
+{
+    IPGlobalProperties properties = IPGlobalProperties.GetIPGlobalProperties();
+    TcpConnectionInformation[] connections = properties.GetActiveTcpConnections();
+    RegisterAndLog($"env/systemconnections/all/{prefix}", $"[{prefix}] Active TCP connections", connections.Length);
+    RegisterAndLog($"env/systemconnections/port/{prefix}", $"[{prefix}] Active TCP connections from {port}", connections.Count(x => x.LocalEndPoint.Port == port));
+}
+
+void RegisterAndLog(string name, string description, int value)
+{
+    BenchmarksEventSource.Register(name, Operations.Max, Operations.Max, description, description, "n0");
+    BenchmarksEventSource.Measure(name, value);
+    Console.WriteLine($"{description}: {value}");
+}
