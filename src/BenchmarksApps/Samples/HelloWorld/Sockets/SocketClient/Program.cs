e64cd6572995182921153c7df16f7bfff04ec359
Natalia Kondratyeva <knatalia@microsoft.com>
2023-11-07 21:37:01 +0000

    Add socket hello-world example

---
+++ src/BenchmarksApps/Samples/HelloWorld/Sockets/SocketClient/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using Microsoft.Crank.EventSources;
+
+Console.WriteLine("Socket HelloWorld Client");
+Console.WriteLine("Args: " + string.Join(" ", args));
+
+// Parse arguments (easy way to do it would be via System.CommandLine package).
+var ip = args[0] == "localhost" ? IPAddress.Loopback : IPAddress.Parse(args[0]);
+var port = int.Parse(args[1]);
+
+// Register events for crank to collect.
+// It doesn't really matter what you put in Operations because we are going to report each
+// measurement only once -- it is better to do all post-processing locally,
+// otherwise spamming events could overshadow the actual perf we're measuring.
+BenchmarksEventSource.Register("socket/sent", Operations.Sum, Operations.Sum, "Bytes sent", "Bytes sent", "n0");
+BenchmarksEventSource.Register("socket/recv", Operations.Sum, Operations.Sum, "Bytes received", "Bytes received", "n0");
+
+var bytes = "Hello, World!"u8.ToArray();
+var buffer = new byte[bytes.Length + 1];
+
+// ---
+
+// In a real benchmark, there needs to be a warmup period before we start collecting measurements.
+// But this is just a hello-world, not a real benchmark -- we are not collecting anything meaningful,
+// so there's no point in warming up, looping etc.
+using var client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+await client.ConnectAsync(new IPEndPoint(ip, port));
+
+// I find it useful to output some logs to console to be able to check the progress.
+Console.WriteLine("Connected");
+
+await client.SendAsync(bytes, SocketFlags.None);
+client.Shutdown(SocketShutdown.Send);
+
+var total = 0;
+while (total < buffer.Length)
+{
+    var recv = await client.ReceiveAsync(buffer.AsMemory(total), SocketFlags.None);
+    if (recv == 0)
+    {
+        break;
+    }
+    total += recv;
+}
+client.Dispose();
+
+// ---
+
+// Report measurements to crank. I find it useful to output them to console as well.
+BenchmarksEventSource.Measure("socket/sent", bytes.Length);
+Console.WriteLine("socket/sent: " + bytes.Length);
+
+BenchmarksEventSource.Measure("socket/recv", total);
+Console.WriteLine("socket/recv: " + total);
---
+++ src/BenchmarksApps/Samples/HelloWorld/Sockets/SocketServer/Program.cs
@@ -0,0 +1,76 @@
+﻿using System.Buffers;
+using System.Collections.Concurrent;
+using System.Net;
+using System.Net.Sockets;
+
+Console.WriteLine("Socket Echo Server");
+Console.WriteLine("Args: " + string.Join(" ", args));
+var ip = args[0] == "localhost" ? IPAddress.Loopback : IPAddress.Parse(args[0]);
+var port = int.Parse(args[1]);
+
+using var server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+server.Bind(new IPEndPoint(ip, port));
+
+server.Listen();
+
+Console.WriteLine("Server started");
+
+// we need this to prevent GC from collecting unawaited tasks
+var _requestRefs = new ConcurrentDictionary<long, RequestRef>();
+var nextId = 0;
+
+while (true)
+{
+    var accept = await server.AcceptAsync();
+    accept.NoDelay = true;
+
+    var requestRef = new RequestRef(nextId++);
+    _requestRefs.TryAdd(requestRef.Id, requestRef);
+
+    requestRef.Request = HandleRequestAsync(requestRef.Id, accept);
+}
+
+// ---
+
+async Task HandleRequestAsync(long id, Socket socket)
+{
+    byte[]? buffer = null;
+    try
+    {
+        buffer = ArrayPool<byte>.Shared.Rent(64*1024);
+        var total = 0;
+        while (total < buffer.Length)
+        {
+            var recv = await socket.ReceiveAsync(buffer.AsMemory(total), SocketFlags.None);
+            if (recv == 0)
+            {
+                break;
+            }
+            total += recv;
+        }
+
+        await socket.SendAsync(buffer.AsMemory(0, total), SocketFlags.None); // echo
+    }
+    finally
+    {
+        if (buffer != null)
+        {
+            ArrayPool<byte>.Shared.Return(buffer);
+        }
+
+        // the way Kestrel does it
+        try
+        {
+            socket.Shutdown(SocketShutdown.Both);
+        }
+        catch { } // ignore
+        socket.Dispose();
+
+        _requestRefs.TryRemove(id, out _); // clean up when finished
+    }
+}
+
+record RequestRef(long Id)
+{
+    public Task? Request { get; set; }
+}
